8bebf1f9cc05f9e01930b3ae98a1303b1432171d
Alevtina Kataeva <aljakataeva@gmail.com>
2020-10-30 11:06:56 +0200

    fixed after first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,32 @@
-let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+
+let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close');
-let saveButton = document.querySelector('.popup__button');
+
+let popupForm = document.querySelector('.popup__content');
+
+let titleInput = document.querySelector('.popup__input_title');
+let subtitleInput = document.querySelector('.popup__input_subtitle');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function showClick() {
     popup.classList.add('popup__open');
+    titleInput.setAttribute('value', profileTitle.textContent);
+    subtitleInput.setAttribute('value', profileSubtitle.textContent);
 }
 
 function closeClick() {
     popup.classList.remove('popup__open');
 }
 
-function saveClick() {
-    let titleInput = document.querySelector('.popup__title');
-    let subtitleInput = document.querySelector('.popup__subtitle');
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-    let titleValue = titleInput.value;
-    let subtitleValue = subtitleInput.value;
-    profileTitle.innerHTML = titleValue;
-    profileSubtitle.innerHTML = subtitleValue;
-    titleInput.setAttribute('value', titleValue);
-    subtitleInput.setAttribute('value', subtitleValue);
+function saveClick(event) {
+    event.preventDefault();
+    profileTitle.textContent = titleInput.value;
+    profileSubtitle.textContent = subtitleInput.value;
     closeClick();
 }
 
 editButton.addEventListener('click', showClick);
 closeButton.addEventListener('click', closeClick);
-saveButton.addEventListener('click', saveClick);
+popupForm.addEventListener('submit', saveClick);
