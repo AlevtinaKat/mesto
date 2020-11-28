e2a561dccdfa1399352949dccfe11faeb9fe6102
Alevtina Kataeva <aljakataeva@gmail.com>
2020-11-28 12:28:07 +0200

    third commit for sixth project

--- scripts/index.js
+++ scripts/index.js
@@ -29,8 +29,6 @@ const linkInput = document.querySelector(".popup__input_link");
 const img = document.querySelector(".popup__foto-full");
 const popupFotoTitle = document.querySelector(".popup__foto-title");
 
-const popupForms = document.querySelectorAll(".popup__content");
-
 const initialCards = [
   {
     name: "Архыз",
@@ -87,15 +85,11 @@ function saveClick(event) {
   event.preventDefault();
   profileTitle.textContent = titleInput.value;
   profileSubtitle.textContent = subtitleInput.value;
-  closeClick(getPopupFromChild(event));
+  closeClick(popup);
 }
 
 function clickLike(event) {
-  if (event.target.classList.contains("element__heart_black")) {
-    event.target.classList.remove("element__heart_black");
-  } else {
-    event.target.classList.add("element__heart_black");
-  }
+  event.target.classList.toggle("element__heart_black");
 }
 
 function removeElement(event) {
@@ -143,7 +137,7 @@ function createFotoElement(elem) {
 
   element.querySelector(".element__heart").addEventListener("click", clickLike);
 
-  addElement(element);
+  return element;
 }
 
 function addElement(element) {
@@ -156,87 +150,28 @@ function showPictureClick() {
 
 function savePictureClick(event) {
   event.preventDefault();
-  createFotoElement({ name: placeInput.value, link: linkInput.value });
-  closeClick(getPopupFromChild(event));
+  addElement(
+    createFotoElement({ name: placeInput.value, link: linkInput.value })
+  );
+  closeClick(popupPicture);
 }
 
-initialCards.forEach(createFotoElement);
+initialCards.forEach((initialCard) =>
+  addElement(createFotoElement(initialCard))
+);
 
 editButton.addEventListener("click", showClick);
-closeButton.addEventListener("click", (event) =>
-  closeClick(getPopupFromChild(event))
-);
+closeButton.addEventListener("click", () => closeClick(popup));
 popupForm.addEventListener("submit", saveClick);
 
 addButton.addEventListener("click", showPictureClick);
-closePictureButton.addEventListener("click", (event) =>
-  closeClick(getPopupFromChild(event))
-);
+closePictureButton.addEventListener("click", () => closeClick(popupPicture));
 popupPictureForm.addEventListener("submit", savePictureClick);
 
-closeFotoButton.addEventListener("click", (event) =>
-  closePopup(getPopupFromChild(event))
-);
+closeFotoButton.addEventListener("click", () => closePopup(popupFoto));
 
 function getPopupFromChild(event) {
   return event.target.closest(".popup");
-}
-
-function setButtonState(button, isActive) {
-  if (isActive) {
-    button.classList.remove("popup__button_invalid");
-    button.disabled = false;
-  } else {
-    button.classList.add("popup__button_invalid");
-    button.disabled = true;
-  }
-}
-
-function showError(form, input) {
-  const error = form.querySelector(`#${input.name}-error`);
-  error.textContent = input.validationMessage;
-  input.classList.add("popup__input_state_invalid");
-}
-
-function hideError(parent, input) {
-  const error = parent.querySelector(`#${input.name}-error`);
-  error.textContent = "";
-  input.classList.remove("popup__input_state_invalid");
-}
-
-function checkValidity(input) {
-  if (input.validity.valueMissing) {
-    input.setCustomValidity("Вы пропустили это поле.");
-  }
-
-  if (input.name === "link" && input.validity.patternMismatch) {
-    input.setCustomValidity("Введите адрес сайта.");
-  }
-}
-
-function checkInputValidity(form, input) {
-  input.setCustomValidity("");
-  checkValidity(input);
-
-  if (input.validity.valid) {
-    hideError(form, input);
-  } else {
-    showError(form, input);
-  }
-}
-
-function popupFormValidation(form) {
-  const popupButton = form.querySelector(".popup__button");
-  setButtonState(popupButton, form.checkValidity());
-
-  const popupInputs = form.querySelectorAll(".popup__input");
-
-  popupInputs.forEach((input) => {
-    input.addEventListener("input", () => {
-      checkInputValidity(form, input);
-      setButtonState(popupButton, form.checkValidity());
-    });
-  });
 }
 
 function closePopupEsc(event) {
@@ -253,5 +188,3 @@ function closePopupOverlay(event) {
     closeClick(event.target);
   }
 }
-
-popupForms.forEach(popupFormValidation);
